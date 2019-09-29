5330ce89ff756b1401a04b1d52aa9b1d08d3ad0c
pete <petermuroza@gmail.com>
2019-09-29 22:39:26 +1000

    setting module called by traffic_monitor_all

---
+++ setting.py
@@ -0,0 +1,5 @@
+# Common Setting for monitoring module.
+
+MONITOR_PERIOD = 10                                     # For monitoring traffic
+
+TOSHOW = True                                           # For showing information in terminal
